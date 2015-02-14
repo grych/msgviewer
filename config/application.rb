b971ba2ffa420ac3aabd2db4f49c1ce2f149e9c8
Tomek Gryszkiewicz <grych@tg.pl>
2015-02-14 21:52:04 +0100

    1.0

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,9 @@ module Msgviewer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.autoload_paths += %W(#{config.root}/lib)
+
   end
 end
